5e038c81d3e3c03b1d3902376b256e07375cdce7
LiubovTytianevych <kydraha1993@gmail.com>
2023-04-19 21:51:30 -0700

    gallery1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const itemUl = document.querySelector('.gallery');
-const showGallery = galleryItems.map(({preview, original, description}) => {
-  return `<li class="gallery__item">
-            <a class="gallery__link" href="${original}">
-              <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
-            </a>
-          </li>`;
-}).join('');
 
-itemUl.insertAdjacentHTML('beforeend', showGallery);
-
-itemUl.addEventListener('click', onOpenModal);
-
-function onOpenModal(event) {
+const galleryEl = document.querySelector('.gallery');
+const markupGallery = galleryItems.map(({ preview, original, description }) => 
+`<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`).join('');
+galleryEl.insertAdjacentHTML('beforeend', markupGallery);
+galleryEl.addEventListener('click', onOpenModal);
+function onOpenModal(event){
   event.preventDefault();
-  
-  if (event.target.nodeName !== 'IMG') {
-    return;
+  const currentItem = event.target;
+  if(currentItem.nodeName !== 'IMG'){
+    return
   }
-  
-  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" alt="${event.target.alt}">`);
+  const instance = basicLightbox.create(`<img class="gallery__image" src="${currentItem.parentNode.href}"/>`);
   instance.show();
 }
+ 
