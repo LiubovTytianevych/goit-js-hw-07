3cc925aeb21a9aca3ba93554a763f4b8f070f0bb
LiubovTytianevych <kydraha1993@gmail.com>
2023-04-23 21:28:30 -0700

    added open modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,40 @@
+
 import { galleryItems } from './gallery-items.js';
+const galleryListEl = document.querySelector('.gallery');
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
-const markupGallery = galleryItems.map(({ preview, original, description }) => 
-`<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`).join('');
-galleryEl.insertAdjacentHTML('beforeend', markupGallery);
-galleryEl.addEventListener('click', onOpenModal);
-function onOpenModal(event){
-  event.preventDefault();
-  const currentItem = event.target;
-  if(currentItem.nodeName !== 'IMG'){
-    return
-  }
-  const instance = basicLightbox.create(`<img class="gallery__image" src="${currentItem.parentNode.href}"/>`);
-  instance.show();
+
+const makeGalleryCard = ({ preview, original } = {}) => {
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="Image description"
+  />
+  </a>
+  </div>
+  `;
+};
+
+
+const galleryCardsArr = galleryItems.map((el) => makeGalleryCard(el));
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryCardsArr.join(''));
+
+galleryListEl.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) {
+event.preventDefault();
+
+const currentItem = event.target;
+if (currentItem.nodeName !== "IMG") {
+  return;
+}
+const instance = basicLightbox.create
+(`<img class="gallery__image" src="${currentItem.dateset.source}" width="800" heagth="600">`)
+instance.show()
+console.dir(currentItem.nodeName)
 }
