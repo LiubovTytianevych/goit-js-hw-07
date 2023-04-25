ba266f611e1d301d989df66d77bb1e4f4e908011
LiubovTytianevych <kydraha1993@gmail.com>
2023-04-24 21:55:43 -0700

    added lightbox2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,20 +21,16 @@ const makeGalleryCard = ({ preview, original } = {}) => {
 
 
 const galleryCardsArr = galleryItems.map((el) => makeGalleryCard(el));
-
 galleryListEl.insertAdjacentHTML('afterbegin', galleryCardsArr.join(''));
-
 galleryListEl.addEventListener('click', onOpenModal);
-
 function onOpenModal(event) {
-event.preventDefault();
-
-const currentItem = event.target;
-if (currentItem.nodeName !== "IMG") {
-  return;
-}
-const instance = basicLightbox.create
-(`<img class="gallery__image" src="${currentItem.dateset.source}" width="800" heagth="600">`)
-instance.show()
-console.dir(currentItem.nodeName)
+  event.preventDefault();
+  const currentItem = event.target;
+  if (currentItem.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img class="gallery__image" src="${currentItem.dataset.source}" width="800" heagth="600">`
+  );
+  instance.show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryContainer = document.querySelector(".gallery");
-const galleryMarkup =galleryItems.map(({preview, originals, description}) =>  {
-  return `
-<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      alt="${description}"
-    />
-  </a>
-</li>`;
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", onOpenModal);
+
+function createGalery(galleryItems) {
+  return galleryItems
+  .map(({preview, original, description }) => {
+    return`
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+  })
+  .join('');
 }
-).join("");
 
-galleryContainer.innerAdjacentHTML('beforeend', markupGallery);
- galleryContainer.addEventListener('click', onOpenModal);
- function onOpenModal(event) {
-  event.preventDefauit();
-
-  const lightbox = new SimpleLightbox('.gallery .gallery__link', {
-    captionData: 'alt',
-    captionDelay: 250
-  });
- }
-console.log(galleryItems);
+const modal = new SimpleLightbox(".gallery .gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
